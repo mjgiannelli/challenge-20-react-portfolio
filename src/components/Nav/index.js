3c6dee422ad91b6ee7cb2367b247621a63b33eb0
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-06 17:25:41 -0500

    add nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+const Nav = () => {
+    return(
+        <header>
+        <h1>
+          <a href="https://mjgiannelli.github.io/mark-giannelli-portfolio/">Mark Giannelli <br /> Portfolio</a>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#about-me">About Me</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#Resume">Resume</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+
+export default Nav;
