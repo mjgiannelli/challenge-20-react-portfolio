adfc88d218091bd36f2f7f1775c7a8ea0349e0fc
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-07 11:49:01 -0500

    deconstruct formState object

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,15 +38,15 @@ const Contact = () => {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={formState.name} onBlur={handleChange} />
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={formState.email} onBlur={handleChange} />
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={formState.message} onBlur={handleChange} />
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                 </div>
                 {errorMessage && (
                     <div>
