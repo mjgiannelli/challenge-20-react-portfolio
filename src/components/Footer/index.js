1c6c9a27c26c15e76538437656a7dd771760e61b
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-06 19:50:12 -0500

    add social media icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,16 @@
+import { SocialIcon } from 'react-social-icons';
+
 function Footer() {
     return (
       <footer>
         <h5>
           <span>&copy;</span> 2021 Designs by Gia, LLC.
         </h5>
+        <div>
+            <SocialIcon url='https://twitter.com/MarkGiannelli2' />
+            <SocialIcon url='https://github.com/mjgiannelli' />
+            <SocialIcon url='https://www.linkedin.com/in/mark-giannelli-mba-458585108/' />
+        </div>
       </footer>
     )
   }
