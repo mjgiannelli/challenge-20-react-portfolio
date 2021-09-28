e20b9546ea6ca7da81a61db0e61bd220d60078eb
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-28 18:04:45 -0500

    add signature to baseball card component

--- src/components/About/BaseballCard/index.js
+++ src/components/About/BaseballCard/index.js
@@ -2,7 +2,16 @@ const BaseballCard = () => {
     return(
         <div id='card-container'>
             <div id='card-body'>
-
+                <div id='card-signature-div'>
+                    <div id='card-signature'>
+                        <p id='print-name'>
+                            MARK GIANNELLI
+                        </p>
+                        <p id='cursive-name'>
+                            Mark Giannelli
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
