eb9b5861af1be9e2846f5d742edff3bd4785cb4d
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-28 18:34:20 -0500

    add topps logo to card

--- src/components/About/BaseballCard/index.js
+++ src/components/About/BaseballCard/index.js
@@ -1,7 +1,10 @@
 const BaseballCard = () => {
-    return(
+    return (
         <div id='card-container'>
             <div id='card-body'>
+                <div id='topps-div'>
+                <img id='topps' src="https://www.logosurfer.com/wp-content/uploads/2018/03/topps-logo_0.png"  alt="Topps Logo" />
+                </div>
                 <div id='card-signature-div'>
                     <div id='card-signature'>
                         <p id='print-name'>
