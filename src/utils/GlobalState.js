5dc5861d233e47901b271d172d7ef8cb4536e069
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-06 23:38:03 -0500

    remove console.logs

--- src/utils/GlobalState.js
+++ src/utils/GlobalState.js
@@ -3,16 +3,12 @@ import { useColorThemeReducer } from "./reducers";
 
 const StoreContext = createContext();
 
-console.log('store context', StoreContext);
-
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useColorThemeReducer({
         colorTheme: 'dark'
     });
-
-    console.log('state', state);
 
     return <Provider value={[state, dispatch]} {...props} />;
 };
