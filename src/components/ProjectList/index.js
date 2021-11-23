8c4d95f058e383ceca1fb93e75f3397b1894a917
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-22 23:02:01 -0600

    add problem solved and tech used to projects state

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -9,31 +9,43 @@ const ProjectList = () => {
         {
             name: 'board-reactions',
             deploy: 'https://board-reactions.herokuapp.com/',
+            problem_solved: 'Connecting board game enthusiasts.',
+            technologies_used: 'MERN Stack, JavaScript, GraphQL.',
             github: 'https://github.com/JSheleg/board-reactions'
         },
         {
             name: 'movie-date',
             deploy: 'https://mjgiannelli.github.io/movie_date/',
+            problem_solved: 'Giving couples ideas on what movies to watch.',
+            technologies_used: 'JavaScript, jQuery, Client Side APIs, UI-Kit.',
             github: 'https://github.com/mjgiannelli/movie_date'
         },
         {
             name: 'note-taker',
             deploy: 'https://shrouded-escarpment-97500.herokuapp.com/',
+            problem_solved: 'Convienient way for small businesses to organize tasks.',
+            technologies_used: 'Node, Express, JavaScript, Database, RESTful API.',
             github: 'https://github.com/mjgiannelli/challenge-11-note-taker'
         },
         {
             name: 'pizza-hunt',
             deploy: 'https://whispering-shelf-04252.herokuapp.com/',
+            problem_solved: 'Helping a small business create a database of people\'s favorite pizzas.',
+            technologies_used: 'MongoDB, Express, Node, JavaScript, RESTful API.',
             github: 'https://github.com/mjgiannelli/pizza-hunt'
         },
         {
             name: 'tech-blog',
             deploy: 'https://polar-woodland-67241.herokuapp.com/',
+            problem_solved: 'Providing a platform for devs to discuss everything tech.',
+            technologies_used: 'SQL, Node, Express, JavaScript, RESTful API.',
             github: 'https://github.com/mjgiannelli/tech-blog'
         },
         {
             name: 'weather-dash',
             deploy: 'https://mjgiannelli.github.io/challenge-6-weather-dashboard/',
+            problem_solved: 'Allows user to search current weather and 5-day forecast for any city.',
+            technologies_used: 'JavaScript, HTML, CSS, Client Side API.',
             github: 'https://github.com/mjgiannelli/challenge-6-weather-dashboard'
         }
     ])
