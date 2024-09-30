0dee54092dab18764b338e3021fb735e94250d14
Mark Giannelli <giannellimj@gmail.com>
2024-09-30 07:11:02 -0500

    update spelling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,7 @@ const Portfolio = () => {
     return (
         <section className='box'>
             <h2 className='title'>Portfolio</h2>
-            <h2 style={{marginBottom: "5rem", color: 'red', textAlign: 'center'}}>Please give time for Render server to kick on when visiting the deployed sight. Could take up to 5 mins, but hey it's free!</h2>
+            <h2 style={{marginBottom: "5rem", color: 'red', textAlign: 'center'}}>Please give time for Render server to kick on when visiting the deployed site. Could take up to 5 mins, but hey it's free!</h2>
             <ProjectList />
         </section>
     )
